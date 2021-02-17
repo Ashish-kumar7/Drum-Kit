228795cd46c346773f0ce596933fa3eee3324d0b
ashish007kumar <ashishkumar357ak@gmail.com>
2021-02-18 02:20:40 +0530

    javascript files

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,5 @@
 console.log("Ashish is Awesome");
-
 window.addEventListener('keydown', function (e) {
-    //  console.log(e);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
@@ -12,15 +10,13 @@ window.addEventListener('keydown', function (e) {
     audio.play();
     key.classList.add('playing');
 });
-
 function removeTransition(e) {
-    //console.log(e);
+
     if (e.propertyName !== 'transform') {
         return;
     }
     this.classList.remove('playing');
 }
-
 const keys = document.querySelectorAll('.key');
 keys.forEach(function (key) {
     key.addEventListener('transitionend', removeTransition);
